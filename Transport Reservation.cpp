0c582f6f7d2ee4e801c4566f664b34daa4fec446
nvardp <72966302+nvardp@users.noreply.github.com>
2021-08-12 14:55:30 +0400

    Add files via upload

---
+++ Transport Reservation.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include "errors.h"
+#include "bus.h"
+
+const std::string ADD = "add";
+const std::string SEARCH = "search";
+const std::string RESERVE = "reserve";
+const std::string INFO = "info";
+const std::string AVAILABLE_BUSSES = "buses available";
+const std::string EXIT = "exit";
+
+
+
+
+bool execute(std::string& command, Bus& bus)
+{
+	for (int i = 0; i < command.size(); i++)
+	{
+		command[i] = tolower(command[i]);
+	}
+
+	if (command == ADD)
+	{
+		if (bus.add_new_bus())
+		{
+			std::cout << SUCCESS;
+			return true;
+		}
+		else
+		{
+			std::cout << ERRORS::not_integer();
+			return false;
+		}
+	}
+	else if (command == SEARCH)
+	{
+		auto number = Bus::get_target_number("Enter bus number ");
+		if (number)
+		{
+			std::cout << bus.bus_info(number);
+			return true;
+		}
+		else
+		{
+			std::cout << ERRORS::not_integer();
+			return false;
+		}
+	}
+	else if (command == RESERVE)
+	{
+		auto b_number = Bus::get_target_number("Enter bus number ");
+		auto s_number = Bus::get_target_number("Enter seat number ");
+		if (s_number > MAX_SEATS || s_number <= 0)
+		{
+			std::cout << ERRORS::out_of_range();
+		}
+		else if (b_number && s_number)
+		{
+			std::cout << bus.reserve(b_number, s_number);
+		}
+		else
+		{
+			std::cout << ERRORS::not_integer();
+		}
+			return true;
+			
+	}
+	else if (command == INFO)
+	{
+		auto b_number = Bus::get_target_number("Enter bus number ");
+		if (b_number)
+			std::cout << bus.info(b_number);
+		else std::cout << ERRORS::not_integer();
+		return true;
+	}
+	else if (command == AVAILABLE_BUSSES)
+	{
+		std::cout << bus.available_buses();
+	}
+	else
+	{
+		std::cout << ERRORS::wrong_command();
+		return false;
+	}
+}
+
+void rules()
+{
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, 8);
+	std::string str = "1. to add a new bus write <<add>>\n";
+	str += "2. to search for information about certain bus write <<search>>\n";
+	str += "3. to reserve a seat write <<reserve>>\n";
+	str += "4. to search for information about certain bus with seats write <<info>>\n";
+	str += "5. to see available busses write <<buses available>>\n";
+	str += "6. to stop the program write <<exit>>\n";
+	std::cout << str;
+	SetConsoleTextAttribute(hConsole, 15);
+}
+
+
+
+
+int main()
+{
+	Bus bus;
+	std::string command{};
+	rules();
+	while (command != EXIT)
+	{
+		std::cout << "----type your command----\n";
+		std::getline(std::cin, command);
+		if (execute(command, bus))
+		{
+			std::cin.clear();
+			std::cin.ignore();
+		}
+
+	}
+}
+
---
+++ bus.cpp
@@ -0,0 +1,160 @@
+#include "bus.h"
+
+Bus::Bus() :
+	number(0), seats{}, driver_name{}, departure_time{},
+	arrival_time{}, starting_point{}, destination{}, existing_bus_numbers{}
+{};
+Bus::Bus(int other) :
+	number(other), seats{}, driver_name{}, departure_time{},
+	arrival_time{}, starting_point{}, destination{}, existing_bus_numbers{}
+{};
+
+int Bus::get_target_number(std::string show_what)
+{
+	std::cout << show_what;
+	int tmp;
+	std::cin >> tmp;
+	if (std::cin.fail() || tmp < 0)
+	{
+		std::cin.clear();
+		std::cin.ignore(1000, '\n');
+		return 0;
+	}
+	else
+		return tmp;
+}
+
+
+bool Bus::add_new_bus()
+{
+	number = get_target_number("Bus number	");
+	if (!number)
+		return false;
+
+	std::cout << "driver name	";
+	std::cin >> driver_name;
+	std::cout << "departure time	";
+	std::cin >> departure_time;
+	std::cout << "arrival time	";
+	std::cin >> arrival_time;
+	std::cout << "starting point	";
+	std::cin >> starting_point;
+	std::cout << "destination	";
+	std::cin >> destination;
+	auto reply = existing_bus_numbers.insert(std::pair<int, Bus>(number, *this));
+	if (reply.second == false)
+		std::cout << ERRORS::existing_bus();
+	return true;
+}
+
+
+std::string Bus::bus_info(int number) const
+{
+
+	auto search_index = existing_bus_numbers.find(number);
+	if (search_index != existing_bus_numbers.end())
+	{
+		std::string result{ "Bus number " };
+		result += std::to_string((search_index->second).get_number());
+		result += "\nDriver name ";
+		result += (search_index->second).get_driver_name();
+		result += "\nDeparture time ";
+		result += (search_index->second).get_departure_time();
+		result += "\nArrival time ";
+		result += (search_index->second).get_arrival_time();
+		result += "\nStarting point ";
+		result += (search_index->second).get_starting_point();
+		result += "\nDestination ";
+		result += (search_index->second).get_destination();
+		result += "\n";
+		return 	 result;
+	}
+	else return ERRORS::wrong_bus_number();
+}
+
+
+std::string Bus::reserve(int bus_number, int seat_number)
+{
+	auto search_index = existing_bus_numbers.find(bus_number);
+	if (search_index != existing_bus_numbers.end())
+	{
+		if ((search_index->second).seats[seat_number - 1])
+			return ERRORS::reserved_seat_number();
+		else {
+			(search_index->second).seats[seat_number - 1] = true;
+			return SUCCESS;
+		}
+	}
+	else return ERRORS::wrong_bus_number();
+}
+
+std::string Bus::info(int bus_number) const
+{
+	auto search_index = existing_bus_numbers.find(bus_number);
+	if (search_index != existing_bus_numbers.end())
+	{
+		std::string result{ "Bus number " };
+		result += std::to_string((search_index->second).get_number());
+		result += "\nDriver name ";
+		result += (search_index->second).get_driver_name();
+		result += "\nDeparture time ";
+		result += (search_index->second).get_departure_time();
+		result += "\nArrival time ";
+		result += (search_index->second).get_arrival_time();
+		result += "\nStarting point ";
+		result += (search_index->second).get_starting_point();
+		result += "\nDestination ";
+		result += (search_index->second).get_destination();
+		result += "\n\nAvailable seats ";
+		for (int i = 0; i < MAX_SEATS; i++)
+		{
+			if (!(search_index->second).seats[i])
+			{
+				result += std::to_string(i + 1);
+				result += " ";
+			}
+		}
+		return 	 result += "\n";
+	}
+	else return ERRORS::wrong_bus_number();
+}
+
+std::string Bus::available_buses() const
+{
+	std::string result{};
+	for (auto it = existing_bus_numbers.begin(); it != existing_bus_numbers.end(); ++it)
+	{
+		if (!(it->second).seats.all())
+		{
+			result += std::to_string(it->first);
+			result += " ";
+		}
+	}
+	return result == "" ? "NO AVAILABLE SEATS.\n" : result;
+}
+
+
+unsigned Bus::get_number() const
+{
+	return number;
+}
+std::string Bus::get_driver_name() const
+{
+	return driver_name;
+}
+std::string Bus::get_departure_time() const
+{
+	return Bus::departure_time;
+}
+std::string Bus::get_arrival_time() const
+{
+	return arrival_time;
+}
+std::string Bus::get_starting_point() const
+{
+	return starting_point;
+}
+std::string Bus::get_destination() const
+{
+	return 	 destination;
+}
